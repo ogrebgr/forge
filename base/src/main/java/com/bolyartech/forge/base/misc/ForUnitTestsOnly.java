51e15029ed0578bce6741280591c94b7c44f42eb
Ognyan Bankov <ogibankov@gmail.com>
2016-03-06 08:51:06 +0200

    cosmetics

--- base/src/main/java/com/bolyartech/forge/base/misc/ForUnitTestsOnly.java
+++ base/src/main/java/com/bolyartech/forge/base/misc/ForUnitTestsOnly.java
@@ -20,10 +20,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by ogre on 2015-10-12
- */
-
 
 /**
  * Marks methods that exist solely to be used in unit test and NOT in normal code

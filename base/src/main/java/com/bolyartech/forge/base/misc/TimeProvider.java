78e3a91e4b00dd2ebeaf93f664af822c6f96041d
Ognyan Bankov <ogibankov@gmail.com>
2016-08-30 07:19:11 +0300

    javadoc

--- base/src/main/java/com/bolyartech/forge/base/misc/TimeProvider.java
+++ base/src/main/java/com/bolyartech/forge/base/misc/TimeProvider.java
@@ -2,7 +2,8 @@ package com.bolyartech.forge.base.misc;
 
 /**
  * This interface abstracts getting time for time measurements in order to provide
- * standard OS independent way of getting time in milliseconds.
+ * standard OS independent way of getting time in milliseconds. This is not wall clock time but real time,
+ * i.e. time measured since VM is started for example
  */
 public interface TimeProvider {
     /**

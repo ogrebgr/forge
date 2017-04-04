4e32569dd527c8a843766990746ae299cee46919
Ognyan Bankov <ogibankov@gmail.com>
2017-04-04 15:31:41 +0300

    fixing tests

--- base/src/test/java/com/bolyartech/forge/base/exchange/HttpExchangeTest.java
+++ base/src/test/java/com/bolyartech/forge/base/exchange/HttpExchangeTest.java
@@ -1,12 +1,19 @@
 package com.bolyartech.forge.base.exchange;
 
-import com.bolyartech.forge.base.http.HttpFunctionalityImpl;
 import com.bolyartech.forge.base.test_utils.StringResultProducer;
-import okhttp3.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,7 +22,6 @@ public class HttpExchangeTest {
     @Test
     public void testExecute() throws IOException, ResultProducer.ResultProducerException {
         OkHttpClient ok = mock(OkHttpClient.class);
-        HttpFunctionalityImpl http = new HttpFunctionalityImpl(ok);
 
         Request req = new Request.Builder().url("http://test").build();
 
@@ -31,7 +37,7 @@ public class HttpExchangeTest {
         when(call.execute()).thenReturn(resp);
         when(ok.newCall(req)).thenReturn(call);
 
-        HttpExchange<String> x = new HttpExchange<>(http, req, new StringResultProducer());
+        HttpExchange<String> x = new HttpExchange<>(ok, req, new StringResultProducer());
         String rez = x.execute();
         Assert.assertTrue("Unexpected result", rez.equals(respBody));
     }

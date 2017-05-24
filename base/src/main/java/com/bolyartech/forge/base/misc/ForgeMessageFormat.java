98a13b5fb3d031ca8c2f1ee94ec829a656d1281d
Ognyan Bankov <ogibankov@gmail.com>
2017-05-24 10:17:05 +0300

    adding file

---
+++ base/src/main/java/com/bolyartech/forge/base/misc/ForgeMessageFormat.java
@@ -0,0 +1,26 @@
+package com.bolyartech.forge.base.misc;
+
+import org.slf4j.helpers.MessageFormatter;
+
+
+/**
+ * Utility class for formatting messages
+ */
+public class ForgeMessageFormat {
+    /**
+     * Formats a message using SLF4J notation
+     *
+     * @param msg    Message with placeholders for values in SLF4J format
+     * @param params parameters to be used to fill the placeholders
+     * @return formatted message
+     */
+    public static String format(String msg, Object... params) {
+        if (params.length == 1) {
+            return MessageFormatter.format(msg, params[0]).getMessage();
+        } else if (params.length > 1) {
+            return MessageFormatter.arrayFormat(msg, params).getMessage();
+        } else {
+            return MessageFormatter.format(msg, params).getMessage();
+        }
+    }
+}
